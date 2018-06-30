f13e453974b9cf5f194ce55ce8ac541dbb7c55b5
Xanthyr <francom.jared@gmail.com>
2018-06-29 17:19:36 -0700

    Refactoring of redundant code and adding extra autonomous rolls features.

--- A_Fine_Adventure/Fightclub.modules.rb
+++ A_Fine_Adventure/Fightclub.modules.rb
@@ -1,77 +1,185 @@
-
-module Rng # RNG engine via dice emulation
-  def diceroll(rolls, dice)
+# RNG engine via dice emulation
+module Rng
+def diceroll(rolls, dice)
   allRolls = []
  rolls.times do
    result = 1 + rand(dice)
    allRolls.push(result)
-   @rolls = allRolls
+   @list_rolls = allRolls
    @allRolls = allRolls.sum
-   @damage = @allRolls + @ability_mod
-    end
+   @lowestRoll = @list_rolls.min
+ end
+  @damage = @allRolls + @ability_mod.to_i
   end
 end
 
-#  Narration of what the dice are actually doing
-# module Narratable
-#   def rollResult
-# end
+
 # Creation tools
 module Creatable
+  include Rng
   attr_accessor :name, :armor_class, :dice_num, :dice_type, :char_max_hp
+  attr_accessor :strength, :dexterity, :constitution, :intelligence, :wisdom, :charisma
+  BASE_STATS = {
+      max_hp: 10,
+      ability_mod: 1,
+      :strength => 0,
+      :dexterity => 0,
+      :constitution => 0,
+      :intelligence => 0,
+      :wisdom => 0,
+      :charisma => 0
+  }
+
+  $character_stats = {
+
+  }
+
+
+  def character_stats
+    puts """
+    STR: #{@strength} [#{self.modifier(self.strength)}]
+    DEX: #{@dexterity} [#{self.modifier(self.dexterity)}]
+    CON: #{@constitution} [#{self.modifier(self.constitution)}]
+    INT: #{@intelligence} [#{self.modifier(self.intelligence)}]
+    WIS: #{@wisdom} [#{self.modifier(self.wisdom)}]
+    CHA: #{@charisma} [#{self.modifier(self.charisma)}]"""
+  end
+
+  def character_stats_array
+    [@strength, @dexterity, @constitution, @intelligence, @wisdom, @charisma]
+  end
+
+  def modifier_array
+    @mod_array = character_stats_array.map {|stat| modifier(stat)}
+  end
+
+   def base_stats_array
+     ["STR:", "DEX:", "CON:", "INT:", "WIS:", "CHA:"]
+  #   ["STR : #{@strength}",
+  #    "DEX : #{@dexterity}",
+  #    "CON : #{@constitution}",
+  #    "INT : #{@intelligence}",
+  #    "WIS : #{@wisdom}",
+  #    "CHA : #{@charisma}"]
+   end
+
+  def brute_force_stats
+    reroll_counter = 1
+    until character_stats_array.sum >= 90
+      roll_all_stats
+      reroll_counter += 1
+    end
+    $results.push(reroll_counter)
+    reroll_counter
+    $results
+  end
+
+  def roll_hp(dice_num, dice_type, extra = 0)
+    set_hp(diceroll(dice_num, dice_type) + self.constitution.to_i)
+    puts "You rolled a #{dice_num}d#{dice_type} for #{@list_rolls}, with a"
+    print "#{self.modifier(self.constitution)} CON modifier, totalling #{self.hit_points}\n"
+    if extra != 0
+      print "#{extra} flat HP has been added as well, totalling #{self.hit_points += extra}"
+    end
+  end
+
+  def roll_stat
+    diceroll(4, 6)
+    @allRolls - @lowestRoll
+  end
+
+  def roll_all_stats
+    self.strength = (roll_stat)
+    puts "For Strength, you rolled #{@list_rolls}, totaling #{@allRolls}, removing the lowest roll [#{@lowestRoll}] leaves a total of #{@allRolls - @lowestRoll}"
+    self.dexterity = (roll_stat)
+    puts "For Dexeterity, you rolled #{@list_rolls}, totaling #{@allRolls}, removing the lowest roll [#{@lowestRoll}] leaves a total of #{@allRolls - @lowestRoll}"
+    self.constitution = (roll_stat)
+    puts "For Constitution, you rolled #{@list_rolls}, totaling #{@allRolls}, removing the lowest roll [#{@lowestRoll}] leaves a total of #{@allRolls - @lowestRoll}"
+    self.intelligence = (roll_stat)
+    puts "For Intelligence, you rolled #{@list_rolls}, totaling #{@allRolls}, removing the lowest roll [#{@lowestRoll}] leaves a total of #{@allRolls - @lowestRoll}"
+    self.wisdom = (roll_stat)
+    puts "For Wisdom, you rolled #{@list_rolls}, totaling #{@allRolls}, removing the lowest roll [#{@lowestRoll}] leaves a total of #{@allRolls - @lowestRoll}"
+    self.charisma = (roll_stat)
+    puts "For Charisma, you rolled #{@list_rolls}, totaling #{@allRolls}, removing the lowest roll [#{@lowestRoll}] leaves a total of #{@allRolls - @lowestRoll}"
+    puts "#{character_stats_array} totals to #{character_stats_array.sum}"
+  end
+
+  def suggest_class
+    if self.constitution == character_stats_array.min
+      puts "\n**Your stamina is questionable, getting into melee could be an issue.**\n"
+    end
+
+    puts "\n"
+    case
+    when self.strength && self.constitution == character_stats_array.max(2)
+      puts "You are one beefy dude, you look like you could take a mad beating. I suggest being a Barbarian."
+    when self.wisdom && self.dexterity && self.constitution == character_stats_array.max(3)
+      puts "You have a zen air about you, and move very well and have some stamina. I suggest being a Monk."
+    when self.charisma && self.strength == character_stats_array.max(2)
+      puts "Your body stand as strong as your beliefs. I suggest being a Paladin."
+    when self.wisdom && self.dexterity == character_stats_array.max(2)
+      puts "You are agile and have good awareness. I suggest being a Ranger."
+    when self.strength == character_stats_array.max
+      puts "You're pretty strong. You could make a good Barbarian, or Fighter."
+    when self.wisdom && self.constitution == character_stats_array.max(2)
+      puts "You have high endurance and a pension for the divine. I suggest being a cleric."
+    when self.dexterity == character_stats_array.max
+      puts "You can move with grace. You could make a good Rogue, Ranger, Monk or Fighter."
+    when self.intelligence == character_stats_array.max
+      puts "You're pretty smart. You could make a good Wizard"
+    when self.wisdom == character_stats_array.max
+      puts "Your insight has value to it. You could make a good Cleric or Druid."
+    when self.charisma == character_stats_array.max
+      puts "You have strong force of personality and commanding presence. You could make a good Bard, Warlock, or Sorcerer."
+    end
+  end
+
   def equip(name, dice_num, dice_type)
     @weapon, @dice_num, @dice_type = name, dice_num, dice_type
   end
 
-  def set_weapon(weapon)
-    @weapon = weapon
-  end
-
-  def set_armor(armor_class)
-    @armor_class = armor_class
+  def weighted_armor_class
+    @armor_class = 10 + (self.dexterity.to_i - 10) / 2
   end
 
   def weapon
-    puts "#{@weapon} - #{dice_num}d#{dice_type}"
+    puts "#{@weapon} - #{@dice_num}d#{@dice_type}"
   end
 
-  def dice
-    [@dice_num, @dice_type]
+  def set_main_stat
+    @ability_mod = modifier(character_stats_array.max)
+    puts "Your highest ability score is #{self.say_best_stat}#{character_stats_array.max}, which has a #{@ability_mod} modifier."
   end
 
-  def set_dice_num(dice_num)
-    @dice_num = dice_num
+  def say_best_stat
+    base_stats_array[character_stats_array.each_with_index.max[1]]
   end
 
-  def set_dice_type(dice_type)
-    @dice_type = dice_type
-  end
 
-  def set_ability_mod(modifier)
-    @ability_mod = modifier
-  end
   def set_hp(health)
     @hit_points = health
     @char_max_hp = health
   end
+
   def quickstart
     puts "Alright let's get you set up with 100hp, 10 armorclass, and a weak attack (1d4 +1)."
     self.set_hp(100)
-    self.set_armor(10)
-    self.set_ability_mod(1)
     self.equip("Broken Sword", 1, 4)
+    self.roll_all_stats
+    self.set_main_stat
+    self.weighted_armor_class
+    self.suggest_class
   end
 end
+
+# Combat/Object manipulation
 module Combat
   include Rng
-  BASE_STATS = {
-      max_hp: 10,
-      ability_mod: 1
-  }
 
   def Combat.included(mod)
-    attr_accessor :hit_points, :ability_mod
+    attr_accessor :hit_points, :ability_mod, :weapon
   end
+
 
   def initialize_stats(stats)
     @stats = stats
@@ -93,19 +201,21 @@ module Combat
     @hit_points += amount
     @hit_points = [@hit_points, @char_max_hp].min
   end
+
   def basic_attack(target)
     puts "\n#{self.name} attacks #{target.name} with their #{@weapon}!\n"
     diceroll(1, 20)
     if @damage >= target.armor_class
       puts "\n#{self.name} needed to roll at least #{target.armor_class}, and they rolled a #{@damage}! The attack hits!\n"
       diceroll(@dice_num, @dice_type)
-      puts "\n#{self.name} attacks #{target.name} with their #{@weapon} for [#{@rolls}+#{@ability_mod}]=#{@damage} points of damage!\n"
+      puts "\n#{self.name} attacks #{target.name} with their #{@weapon} for [#{@list_rolls}#{@ability_mod}]=#{@damage} points of damage!\n"
       target.hurt(@damage)
       puts "\n"
     else
       puts "\n\n#{self.name} needed to roll at least #{target.armor_class}, but they rolled a #{@damage}! The attack whiffs! finlevThump!\n"
     end
   end
+
   def throwdown(target)
     turns = 1
     puts "\nOh shit! #{self.name} and #{target.name} are throwing down hard!"
@@ -116,6 +226,7 @@ module Combat
       end
       if target.alive? == false
         puts "Yo, #{target.name} just got dunked on super hard! It took #{turns} turns!\n\n"
+        $results.push(self.name)
         puts self.status
         puts target.status
       end
@@ -125,11 +236,42 @@ module Combat
     end
   end
 end
+$results = []
+
+# Change Stat Mods
+module CharacterClasses
+  # include Rng
+  # include Creatable
+
+  BASE_CLASSES = {
+    :barbarian => @strength,
+    :bard => @charisma,
+    :cleric => @wisdom,
+    :druid => @wisdom,
+    :fighter => @strength,
+    :monk => @dexterity,
+    :paladin => @strength,
+    :ranger => @dexterity,
+    :rogue => @dexterity,
+    :sorcerer => @charisma,
+    :warlock => @charisma,
+    :wizard => @intelligence
+  }
+
+  def modifier(score)
+    modifier = (score.to_i - 10) / 2
+    if modifier.positive? || modifier == 0
+      return "+#{modifier}"
+      else return modifier
+    end
+  end
+end
 
 class Dude
   include Combat
   include Rng
   include Creatable
+  include CharacterClasses
   MAX_HP = 9999
   def initialize(name)
     @name = name
@@ -145,10 +287,12 @@ class Dude
     puts "Hitpoints: #{@hit_points}/#{self.char_max_hp}"
     puts "Armor Class: #{@armor_class}"
     puts "#{@weapon} - #{dice_num}d#{dice_type}"
-    puts "Ability Modifier: +#{@ability_mod}"
+    puts "Primary Ability Modifier: #{self.say_best_stat.gsub(":", "")}"
+    puts character_stats
     puts "*" * 80
   end
 end
+
 
 class Target < Dude
 end
@@ -156,7 +300,7 @@ class Wizard < Dude
   def fireball(target)
     puts "#{self.name} is casting Fireball towards #{target.name}!"
     diceroll(3, 6)
-    puts "#{target.name} is fireballed for [#{@rolls}+#{@ability_mod}]=#{@damage} damage!"
+    puts "#{target.name} is fireballed for [#{@list_rolls}+#{@ability_mod}]=#{@damage} damage!"
     target.hurt(@damage)
 
   end
@@ -169,7 +313,7 @@ class Fighter < Dude
     self.basic_attack(target)
   end
     diceroll(1, 4)
-    puts "#{self.name} finishes with a wide swing of the #{@weapon} for [#{@rolls}+#{@ability_mod}]=#{@damage} points of damage. Dang!"
+    puts "#{self.name} finishes with a wide swing of the #{@weapon} for [#{@list_rolls}+#{@ability_mod}]=#{@damage} points of damage. Dang!"
       target.hurt(@damage)
     end
 end
@@ -187,69 +331,7 @@ class Cleric < Dude
     puts "#{self.name} is casting a level #{lvl} Cure Wounds on #{target.name}!"
     diceroll(lvl, 8)
     target.heal(@damage)
-    puts "#{target.name} is healed for #{@rolls}+#{@ability_mod}=#{@damage} health points!"
+    puts "#{target.name} is healed for #{@list_rolls}+#{@ability_mod}=#{@damage} health points!"
   end
 end
 
-def start
-  puts "*" * 80
-  puts "Welcome to DnD Fight Club. Make a dude to fight with. Press Enter to begin."
-  $stdin.gets.chomp
-  creator
-end
-def creator
-  include Rng
-  include Combat
-  puts "*" * 80
-  puts "Okay, first, what will this fighter's name be?\n"
-  name = $stdin.gets.chomp
-  dude1 = Dude.new(name)
-  puts "Alrighty then, welcome to the club #{dude1.name}.\n"
-  puts "Now we have #{dude1.name}, let's describe how they fight. \n\nWhat is the preferred method of attack?\n"
-  dude1.set_weapon($stdin.gets.chomp)
-  puts "And how many dice does this attack require?\n"
-  dude1.set_dice_num($stdin.gets.chomp.to_i)
-  puts "What type of dice does the attack use?\n"
-  dude1.set_dice_type($stdin.gets.chomp.to_i)
-  puts "Excellent, so they fight with their #{dude1.weapon}, and it does #{dude1.dice_num}d#{dude1.dice_type} damage.\n"
-  puts "What about their armor? How much armor class do they have?\n"
-  dude1.set_armor($stdin.gets.chomp.to_i)
-  puts "And finally, how many health points do they have?\n"
-  dude1.set_hp($stdin.gets.chomp.to_i)
-  puts "Wonderful, Phase 1 has been completed.\n"
-
-  puts "*" * 80
-  puts "Now, we have to make an opponent for your guy to fight. What is their name?\n"
-  name = $stdin.gets.chomp
-  dude2 = Dude.new(name)
-  puts "Alrighty then, welcome to the club #{dude2.name}."
-  puts "Now we have #{dude2.name}, let's describe how they fights What is the preferred method of attack?\n"
-  dude2.set_weapon($stdin.gets.chomp)
-  puts "And how many dice does this attack require?\n"
-  dude2.set_dice_num($stdin.gets.chomp.to_i)
-  puts "What type of dice does the attack use?\n"
-  dude2.set_dice_type($stdin.gets.chomp.to_i)
-  puts "Excellent, so they fight with their #{dude2.weapon}, and it does #{dude2.dice_num}d#{dude2.dice_type} damage.\n"
-  puts "What about their armor? How much armor class do they have?\n"
-  dude2.set_armor($stdin.gets.chomp.to_i)
-  puts "And finally, how many health points do they have?\n"
-  dude2.set_hp($stdin.gets.chomp.to_i)
-  puts "Fantastic, that wraps it all up. Let's head to the brawl.\n"
-  puts "*" * 80
-  puts "And now all your hard work has come to fruition! A battle to the death between your two fighters, #{dude1.name} and #{dude2.name}"
-  puts "Prepare your butts, because they are about to get their brawl on!"
-  puts "Press enter to watch the carnage."
-  $stdin.gets.chomp
-  dude1.throwdown(dude2)
-  dude1.status
-  dude2.status
-  puts "Thank you for playing!\n"
-  puts "Would you like to start over? [y/n]"
-  answer = $stdin.gets.chomp.downcase
-  if answer == "y"
-    start
-  else
-    exit
-  end
-end
-# start
